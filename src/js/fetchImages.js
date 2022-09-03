fe3698be0941f358187a9567105ff29337cbac14
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-09-03 16:48:55 +0200

    finish

---
+++ src/js/fetchImages.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export default class ServiceAPI {
+  constructor() {
+      this.options = {
+      params: {
+        key: '29686062-6a845a2f1faff990e4be6d419',
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    };
+  }
+
+  async getPictures() {
+    const response = await axios.get('https://pixabay.com/api/', this.options);
+    this.incrementPage();
+    return response;
+  }
+
+  incrementPage() {
+    this.options.params.page += 1;
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+
+  get searchQuery() {
+    return this.options.params.q;
+  }
+
+  set searchQuery(newQuery) {
+    this.options.params.q = newQuery;
+  }
+
+  get pageNumber() {
+    return this.options.params.page;
+  }
+
+  set pageNumber(newNumber) {
+    this.options.params.page = newNumber;
+  }
+}
+
+
